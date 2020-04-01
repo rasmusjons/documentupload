c9c09ce2d3ffbe95aac1458c438041b8b1f28986
Rasmus <rasmusjons@gmail.com>
2020-04-01 12:59:19 +0200

    routes

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
+// Imports.
 const express = require("express");
 require("./db/mongoose");
-const documentRouter = require("../server/routers/document");
-const serveStatic = require("serve-static");
-const path = require("path");
-
+const history = require("connect-history-api-fallback");
 const app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
-
-const port = process.env.PORT;
-
+// Express server.
+const staticFileMiddleware = express.static(__dirname);
+app.use(staticFileMiddleware);
+app.use(
+  history({
+    disableDotRule: true,
+    verbose: true
+  })
+);
+app.use(staticFileMiddleware);
+const port = process.env.PORT || 5000;
 app.use(documentRouter);
 
 app.listen(port, () => {
-  console.log("Server is up on port " + port);
+  console.log("App listening on port " + port);
 });
