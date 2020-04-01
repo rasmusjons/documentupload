0b3d1c9d2b3c0b5207b248a801a2f843192a0fba
Rasmus <rasmusjons@gmail.com>
2020-04-01 11:08:25 +0200

    routes

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "",
+    path: "/",
     name: "Home",
     component: Home
   },
@@ -20,7 +20,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL,
   routes
 });
 
