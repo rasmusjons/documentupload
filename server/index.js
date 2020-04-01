5472bae01ea2f5527be134e61c8f5b73bda5c757
Rasmus <rasmusjons@gmail.com>
2020-04-01 14:36:44 +0200

    ny server setup

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const express = require("express");
 const history = require("connect-history-api-fallback");
 const app = express();
 // Express server.
-const staticFileMiddleware = express.static(__dirname + "/dist");
+const staticFileMiddleware = express.static(__dirname);
 app.use(staticFileMiddleware);
 app.use(
   history({
